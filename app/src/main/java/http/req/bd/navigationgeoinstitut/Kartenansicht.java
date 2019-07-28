22c3bd8247d2131c556be75162e0a8b19266a14d
Benni Dietz <b_diet05@uni-muenster.de>
2019-07-28 14:25:32 +0200

    Aktivität Kartenansicht hinzugefügt => dorthin navigieren + ggf. Raumnr.

---
+++ app/src/main/java/http/req/bd/navigationgeoinstitut/Kartenansicht.java
@@ -0,0 +1,47 @@
+package http.req.bd.navigationgeoinstitut;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Kartenansicht extends AppCompatActivity {
+
+    String aktuelle_position = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+        setContentView(R.layout.activity_kartenansicht);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        String position = getIntent().getStringExtra("position");
+        if (position != null) {
+            aktuelle_position = position;
+            getSupportActionBar().setSubtitle(position);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            this.finish();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
--- app/src/main/java/http/req/bd/navigationgeoinstitut/Startseite.java
+++ app/src/main/java/http/req/bd/navigationgeoinstitut/Startseite.java
@@ -1,37 +1,69 @@
 package http.req.bd.navigationgeoinstitut;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Startseite extends AppCompatActivity {
 
     TextView main_text;
 
     LinearLayout menü_für_raum;
     LinearLayout menü_allgemein;
 
+    Button btn_aktuelle_position_anzeigen, btn_zu_raum_navigieren, btn_raum_suchen;
+    Context mContext = this;
+
+    String aktuelle_position = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+        setContentView(R.layout.activity_startseite);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         main_text = findViewById(R.id.main_text);
         menü_für_raum = findViewById(R.id.menü_für_raum);
         menü_allgemein = findViewById(R.id.menü_allgemein);
         handleIntent();
+        btn_aktuelle_position_anzeigen = findViewById(R.id.btn_aktuelle_position_anzeigen);
+        btn_aktuelle_position_anzeigen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent zur_karte = new Intent(mContext, Kartenansicht.class);
+                zur_karte.putExtra("position", aktuelle_position); // die Raumnummer übergeben
+                startActivity(zur_karte);
+            }
+        });
+        btn_zu_raum_navigieren = findViewById(R.id.btn_zu_raum_navigieren);
+        btn_zu_raum_navigieren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btn_raum_suchen = findViewById(R.id.btn_raum_suchen);
+        btn_raum_suchen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(mContext, Kartenansicht.class));
+            }
+        });
 
     }
+
+
 
     @Override
     protected void onNewIntent(Intent intent) {
@@ -47,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         Uri appLinkData = appLinkIntent.getData();
         if (appLinkData != null) {
             String room_number = appLinkData.getLastPathSegment();
+            aktuelle_position = room_number;
             main_text.setText("Herzlich willkommen im Geo-Institut!\n\nSie befinden sich gerade vor Raum " + room_number + ".");
             menü_allgemein.setVisibility(View.GONE);
         } else {
